87ed67363a895a51002cbb107c1fdb71ad54ee6d
popopopo11111 <nyunnyu@outlook.jp>
2023-09-11 09:59:14 +0900

    9-4 Web API

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -42,5 +42,14 @@
                 }
             }
         </script>
+        <div>
+            @foreach($questions as $question)
+                <div>
+                    <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                        {{ $question['title'] }}
+                    </a>
+                </div>
+            @endforeach
+        </div>
     </body>
 </html>
